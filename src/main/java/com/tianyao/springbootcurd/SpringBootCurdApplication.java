7ab353582b34bdcb7d034feadc3c4eddd0ba4ded
tianyao1916 <tianyao1916@163.com>
2018-05-07 11:44:18 +0800

    update by tianyao

---
+++ src/main/java/com/tianyao/springbootcurd/SpringBootCurdApplication.java
@@ -0,0 +1,12 @@
+package com.tianyao.springbootcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tianyao/springbootcurd/SpringBootCurdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tianyao.springbootcurd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCurdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
